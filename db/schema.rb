39082cbf2cfc3ddb0f0a9ed6d1ff5a63fc4c9783
smgoy <smgoy31@gmail.com>
2016-08-29 17:41:07 -0700

    creates user model and users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160830003118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "session_token",   null: false
+    t.string   "password_digest", null: false
+    t.string   "f_name",          null: false
+    t.string   "l_name"
+    t.string   "city",            null: false
+    t.text     "description"
+    t.string   "pofile_image"
+    t.boolean  "host",            null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
 
 end
