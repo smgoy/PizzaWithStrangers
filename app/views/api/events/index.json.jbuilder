bfdcca2880278da5f47c0de7249428092e6635bb
smgoy <smgoy31@gmail.com>
2016-09-05 21:31:46 -0700

    begins styling events

--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -2,10 +2,8 @@
   json.set! event.id.to_s do
     json.id event.id
     json.host_id event.host_id
-    json.date event.date.strftime("%a, %B %d")
     json.address event.address
-    json.time event.time.strftime("%I:%M %p")
-    json.image event.image
+    json.time event.time
     json.seats event.seats
     json.name event.name
   end
--- frontend/components/events/events_index.jsx
+++ frontend/components/events/events_index.jsx
@@ -15,29 +15,48 @@ class EventsIndex extends React.Component {
 
   }
 
+  decorateEvent(eventObj) {
+    return(
+      <div>
+        <div className="event-container">
+          <div className="event-description">
+            <p className="desc">Event Name:</p>
+            <p className="desc">6 Seat Left</p>
+          </div>
+          <div className="event-details">
+            <p className="details">9746 Florian Court</p>
+            <p className="details">December, 26 at 08:57 AM</p>
+            <a className="join">Join</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const {events} = this.props;
 
-    const eventList = Object.keys(events).map( id => (
-      <div key={id} className="event-container">
-        <p className="event-text">{events[id].name}</p>
-        <p className="event-text">{events[id].address}</p>
-        <p className="event-text">{events[id].date}</p>
-        <p className="event-text">{events[id].time}</p>
-        <p className="event-text">Available Seats: {events[id].seats}</p>
-        <a data-id={id} onClick={this.joinEvent.bind(this)} className="btn btn-primary">Join the Party</a>
-        </div>
-    ));
+    let thisWeek;
+    let thisMonth;
+    let thisYear;
+
+    Object.keys(events).forEach( id => {
+      //need to get date.js working
+    });
+
 
     return(
       <div className="container">
         <a onClick={this.showEventForm.bind(this)} className="new-event">+ New Event</a>
+
         <div className="event-header-container">
           <h2 className="event-header-text">this week.</h2>
         </div>
+
         <div className="event-header-container">
           <h2 className="event-header-text">this month.</h2>
         </div>
+
         <div className="event-header-container">
           <h2 className="event-header-text">this year.</h2>
         </div>
