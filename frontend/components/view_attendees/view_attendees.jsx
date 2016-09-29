e403c1e0210b219f7b41f6c116d677b28643e7d5
smgoy <smgoy31@gmail.com>
2016-09-28 17:09:56 -0700

    adds modal structure

--- frontend/components/view_attendees/view_attendees.jsx
+++ frontend/components/view_attendees/view_attendees.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
 class ViewAttendees extends React.Component {
-  
+  showModal() {
+      this.setState({show: true});
+    }
+
+    hideModal() {
+      this.setState({show: false});
+    }
+
+    render() {
+      return (
+        <ButtonToolbar>
+          <Button bsStyle="primary" onClick={this.showModal}>
+            See who's attending >
+          </Button>
+
+          <Modal
+            {...this.props}
+            show={this.state.show}
+            onHide={this.hideModal}
+            dialogClassName="custom-modal"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+=             ...
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.hideModal}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </ButtonToolbar>
+      );
+    }
 }
